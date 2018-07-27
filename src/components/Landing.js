7e97f55731b227b45d9704c2b6c4f5cbe04c40d9
RandyGoldsmith <rgoldshz@gmail.com>
2018-07-27 02:31:47 -0500

    making responsive text inside image

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,10 +4,12 @@ import './Landing.css';
 
 const Landing = () => (
 	<section className="landing">
-		<h1 className="hero-title">Turn the music up!</h1>
-		<img src= "https://i2.wp.com/www.audioschurke.de/wp-content/uploads/2018/06/kopfh%C3%B6rer_header.jpg?fit=1920%2C1279&ssl=1" alt="A women wearing headphones" className='image-headphones' />
-
+		<div className="image-wrapper">
+			<img src= "https://i2.wp.com/www.audioschurke.de/wp-content/uploads/2018/06/kopfh%C3%B6rer_header.jpg?fit=1920%2C1279&ssl=1" alt="A women wearing headphones" className='image-headphones' />
+		 		<h1 className="hero-title">Turn the music up!</h1>
+		</div>
 		<section className="selling-points">
+			<div className="container-points">
 			<div className="point">
 				<span className="icon ion-md-musical-notes"></span>
 				<h2 className="point-title">Choose your music</h2>
@@ -22,6 +24,7 @@ const Landing = () => (
 				<span className="icon ion-md-phone-portrait"></span>
 				<h2 className="point-title">Mobile enabled</h2>
 				<p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+			</div>
 			</div>
 		</section>	
 	</section>
